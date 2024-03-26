e126dbc4c08734a2ae77f3225814d25b76b527d2
RyanStud <161963481+RyanStud@users.noreply.github.com>
2024-03-26 14:24:19 -0300

    Commit branch1

---
+++ Menu.java
@@ -0,0 +1,8 @@
+public class Menu {
+    public void imprimirMenu() {
+        System.out.println("Menu de Opções de Cadastro de Usuários:");
+        System.out.println("1. Cadastrar novo usuário");
+        System.out.println("2. Listar usuários cadastrados");
+        System.out.println("3. Sair");
+    }
+}
